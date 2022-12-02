c6f043ebacef3b28630b9dfd2502359968e406ea
Michal Samborski <therealsybarite@gmail.com>
2022-12-02 03:11:04 -0500

    day 1 part 2 complete

--- day1.sh
+++ day1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '{if ($1 !="") s+=$1; else {print s; s=0;}}' input1.txt | sort -rn | head -n 1
+awk '{if ($1 !="") s+=$1; else {print s; s=0;}}' day1input.txt | sort -n | tail -n 3 | awk '{s+=$1} END {print $1, s}'
