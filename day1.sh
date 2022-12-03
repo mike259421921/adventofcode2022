6ba3881f0010e4b54e5739947eae0d472c3508b5
Michal Samborski <48521251+mike259421921@users.noreply.github.com>
2022-12-02 22:32:39 -0500

    Update day1.sh

--- day1.sh
+++ day1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # TODO: last batch of data not included.  else not triggered at end of file.  update if statement or append new line to input
-{if ($1 !="") s+=$1; else {print s; s=0;}}' day1input.txt} | sort -n | tail -n 3 | awk '{s+=$1} END {print $1, s}'
+awk '{if ($1 !="") s+=$1; else {print s; s=0;}}' day1input.txt} | sort -n | tail -n 3 | awk '{s+=$1} END {print $1, s}'
